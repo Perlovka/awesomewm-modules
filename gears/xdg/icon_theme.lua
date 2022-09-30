243813f3cea918d521932fdb21dbfb9f6c076651
Michael Perlov <perlovka@gmail.com>
2022-09-30 13:16:07 +0300

    Refactor, support Gtk 4

--- gears/xdg/icon_theme.lua
+++ gears/xdg/icon_theme.lua
@@ -2,30 +2,35 @@
 --- Icon theme module
 --
 -- @author Michael Perlov
--- @copyright 2020 Michael Perlov
+-- @copyright 2022 Michael Perlov
 -- @module gears.xdg.icon_theme
 ---------------------------------------------------------------------------
 
 local Gtk = require("lgi").Gtk
-local dbg = require("gears.debug")
+local Gio = require("lgi").Gio
 
 local icon_theme = {}
+local is_gtk4 = Gtk.version >= "4"
 
 --- Create new theme object
 --  @param  theme_name  Theme name
 --  @param  icon_size   Preferred icon size
 --  @return icon_theme object
-function icon_theme:new(theme_name, icon_size)
+function icon_theme:new(args)
     local theme = {}
     setmetatable(theme, self)
     self.__index = self
 
-    theme.name = theme_name or nil
-    theme.icon_size = icon_size or 24
+    theme.name = args[1] or args.name or nil
+    theme.icon_size = args[2] or args.icon_size or 24
 
-    if theme_name then
+    if theme.name then
         theme.gtk_theme = Gtk.IconTheme.new()
-        Gtk.IconTheme.set_custom_theme(theme.gtk_theme, theme_name);
+        if not is_gtk4 then
+            Gtk.IconTheme.set_custom_theme(theme.gtk_theme, theme.name);
+        else
+            Gtk.IconTheme.set_theme_name(theme.gtk_theme, theme.name);
+        end
     else
         theme.gtk_theme = Gtk.IconTheme.get_default()
     end
@@ -40,19 +45,35 @@ end
 function icon_theme:get_icon_path(icon_name, fallback_icon_name)
     local icon_path
 
-    -- TODO: use flags
-    local icon_info = Gtk.IconTheme.lookup_icon(self.gtk_theme, icon_name, self.icon_size, 0);
-    -- fallback
-    if not icon_info and fallback_icon_name then
-        icon_info = Gtk.IconTheme.lookup_icon(self.gtk_theme, fallback_icon_name, self.icon_size, 0)
-    end
-    if icon_info then
-        icon_path = Gtk.IconInfo.get_filename(icon_info)
-        if icon_path then
-            return icon_path
+    if not is_gtk4 then
+        -- TODO: use flags
+        local icon_info = Gtk.IconTheme.lookup_icon(self.gtk_theme, icon_name, self.icon_size, 0);
+        -- fallback
+        if not icon_info and fallback_icon_name then
+            icon_info = Gtk.IconTheme.lookup_icon(self.gtk_theme, fallback_icon_name, self.icon_size, 0)
+        end
+        if icon_info then
+            icon_path = Gtk.IconInfo.get_filename(icon_info)
+            if icon_path then
+                return icon_path
+            end
+        end
+    else
+        -- TODO: use flags
+        local icon = Gtk.IconTheme.lookup_icon(self.gtk_theme, icon_name, {}, self.icon_size, 1, 0, 0);
+        -- fallback
+        if not icon and fallback_icon_name then
+            icon = Gtk.IconTheme.lookup_icon(self.gtk_theme, fallback_icon_name, {}, self.icon_size, 1, 0, 0);
+        end
+        if icon then
+            icon_path = Gtk.IconPaintable.get_file(icon)
+            if icon_path then
+                return Gio.File.get_path(icon_path)
+            end
         end
     end
-    return ""
+
+    return "/usr/share/icons/Adwaita/24x24/status/image-loading.png"
 end
 
-return setmetatable(icon_theme, { __call = function(_, ...) return icon_theme:new(...) end })
+return setmetatable(icon_theme, { __call = icon_theme.new })
--- gears/xdg/icon_theme.md
+++ gears/xdg/icon_theme.md
@@ -11,22 +11,15 @@ https://github.com/GNOME/gtk
 ### Usage:
 
 ```lua
-local icon_theme = require("gears.xdg.icon_theme")
-local icon_theme_acyls = icon_theme:new("ACYLS", 24)
---- or
---  local icon_theme_acyls = require("gears.xdg.icon_theme"):new("ACYLS", 24)
---- or
---  local icon_theme_acyls = require("gears.xdg.icon_theme")("ACYLS", 24)
-
-local icon_path = icon_theme_acyls:get_icon_path("folder")
+local icon_theme = require("gears.xdg.icon_theme"){"ACYLS", 24}
+local icon_path = icon_theme:get_icon_path("folder")
 
 ```
 
 [Constructors](#Constructors)
 -----------------------
 
-[:new (theme_name, icon_size)](#:new) &nbsp;&nbsp; Create new theme object  
-[(theme_name, icon_size)](#new) &nbsp;&nbsp; Create new theme object
+[gears.xdg.icon_theme{theme_name, icon_size}](#new) &nbsp;&nbsp; Create new theme object
 
 [Methods](#Methods)
 -----------------------
@@ -35,23 +28,22 @@ local icon_path = icon_theme_acyls:get_icon_path("folder")
 
 ## <a name="Constructors"></a>Constructors
 
-#### <a name=":new"></a>**:new (theme_name, icon_size)**
-#### <a name="new"></a>**(theme_name, icon_size)**
+#### <a name="new"></a>**gears.xdg.icon_theme{theme_name, icon_size}**
     Create new theme object
 #### &nbsp;&nbsp;&nbsp; Parameters:
-* &nbsp; *theme_name* (**string**)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Icon theme name
-* &nbsp; *icon_size* (**string**)&nbsp;&nbsp;Preferred icon size
+* &nbsp; *theme_name* (**string**)&emsp;&emsp;&emsp;&nbsp;Icon theme name
+* &nbsp; *icon_size* (**number**)&emsp;&emsp;&emsp;&emsp;Preferred icon size
 
 ## <a name="Methods"></a>Methods
 
-#### <a name="get_icon_path"></a>**:get_icon_path (icon_name, fallback_icon_name)**
+#### <a name="get_icon_path"></a>**:get_icon_path(icon_name, fallback_icon_name)**
 
     Get icon path by icon name
 
 #### &nbsp;&nbsp;&nbsp; Parameters:
 
-* &nbsp; *icon_name* (**string**)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Icon name
-* &nbsp; *fallback_icon_name* (**string**)&nbsp;&nbsp;If icon not found, return path to icon with this name
+* &nbsp; *icon_name* (**string**)&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Icon name
+* &nbsp; *fallback_icon_name* (**string**)&emsp;&emsp;&nbsp;If icon not found, return path to icon with this name
 
 #### &nbsp;&nbsp;&nbsp; Returns:
 
